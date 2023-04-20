2ad5e0d1d02b26b93572ea49d80f1bbf50dbeabe
irynakrykun <ira_franyk@ukr.net>
2023-04-20 19:45:09 +0300

    post

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,13 +4,14 @@ const { User } = require("../models/user");
 
 const register = async (res, req, next) => {
   try {
+    // const { email, password } = req.body;
     const newUser = await User.create(req.body);
 
     res.status(201).json({
       user: {
         email: newUser.email,
         password: newUser.password,
-        subscription: newUser.subscription,
+        
       },
     });
   } catch (error) {
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const ctrl = require("../../controllers/auth");
+// const jsonParser = express.json();
 
-
-const { validateBody } = require('../../middlewares');
+const { validateBody }  = require('../../middlewares');
 const { schemas }  = require('../../models/user');
 
 router.post("/register",validateBody(schemas.registerSchema),ctrl.register)
